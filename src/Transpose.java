c94cf4c8cca9775593f2e0fc54b240e33cc4103e
yashshukla1319 <81147963+yashshukla1319@users.noreply.github.com>
2021-12-16 16:10:17 +0530

    Linear search, transpose

---
+++ src/Transpose.java
@@ -0,0 +1,32 @@
+//Matrix transpose row <=> column
+public class Transpose {
+        static final int M = 3;
+        static final int N = 4;
+
+        static void transpose(int A[][], int B[][])
+        {
+            int i, j;
+            for (i = 0; i < N; i++)
+                for (j = 0; j < M; j++)
+                    B[i][j] = A[j][i];
+        }
+
+        public static void main (String[] args)
+        {
+            int A[][] = { {1, 1, 1, 1},
+                    {2, 2, 2, 2},
+                    {3, 3, 3, 3}};
+
+            int B[][] = new int[N][M], i, j;
+
+            transpose(A, B);
+
+            System.out.print("Result matrix is \n");
+            for (i = 0; i < N; i++)
+            {
+                for (j = 0; j < M; j++)
+                    System.out.print(B[i][j] + " ");
+                System.out.print("\n");
+            }
+        }
+    }
