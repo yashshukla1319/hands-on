40b0508358da48f6f1a19e6707b81c776d3f9b07
yashshukla1319 <81147963+yashshukla1319@users.noreply.github.com>
2021-12-17 14:08:57 +0530

    stream

--- src/StreamsDemo.java
+++ src/StreamsDemo.java
@@ -1,7 +1,30 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.stream.Collectors;
+
+class Student {
+
+    private Integer id;
+    private String name;
+
+    public Student(Integer id, String name)
+    {
+
+        this.id = id;
+        this.name = name;
+    }
+    public Integer getId()
+    {
+        return id;
+    }
+    public String getName()
+    {
+        return name;
+    }
+}
+
 
 public class StreamsDemo {
     public static void main(String[] arg){
@@ -18,5 +41,21 @@ public class StreamsDemo {
         strlist.forEach(System.out::println);
         //number.stream().map(i->(i*i)).collect(Collectors.toList()).forEach(System.out::println);
         number.forEach(i->System.out.println(i));
+
+        List<Student> lt = new ArrayList<>();
+
+        lt.add(new Student(1, "yash"));
+        lt.add(new Student(2, "ronak"));
+        lt.add(new Student(3, "dhaval"));
+
+        LinkedHashMap<Integer, String>
+                map = lt.stream()
+                .collect(Collectors
+                .toMap(Student::getId, Student::getName, (x, y) -> x + ", " + y, LinkedHashMap::new));
+
+        map.forEach(
+                (x, y) -> System.out.println(x + ": " + y));
     }
+
 }
+
