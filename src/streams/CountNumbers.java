143769d50ad57c9ea73751141542cd4dcab2f887
yash shukla <yash@ifourtechnolab.com>
2024-08-13 17:05:10 +0530

    fix: new code

--- src/streams/CountNumbers.java
+++ src/streams/CountNumbers.java
@@ -3,6 +3,7 @@ package streams;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -29,6 +30,38 @@ public class CountNumbers {
 
         // Print the occurrences
         occurrences.forEach((number, count) -> System.out.println("Element: " + number + ", Count: " + count));
+
+        String input = "hello world";
+
+        // Count the occurrences of each character
+        Map<Character, Long> characterCounts = input.chars()
+                .mapToObj(c -> (char) c) // Convert int to char
+                .collect(Collectors.groupingBy(
+                        Character::charValue, // Group by character
+                        Collectors.counting() // Count occurrences
+                ));
+
+        Map<Character, Long> characterCounts2 = input.chars()
+                .mapToObj(c -> (char) c) // Convert int to char
+                .collect(Collectors.groupingBy(
+                        Character::charValue, // Group by character
+                        Collectors.counting() // Count occurrences
+                ));
+
+        List<String> list = List.of("abc", "abc", "pqr");
+
+        Map<String, Long> stringCounts = list
+                .stream() // Convert int to char
+                .collect(Collectors.groupingBy(
+                        Function.identity(), // Group by character
+                        Collectors.counting() // Count occurrences
+                ));
+
+        //characterCounts.forEach((character, count) ->
+                System.out.println(stringCounts);
+        // Print the result
+        characterCounts.forEach((character, count) ->
+                System.out.println(character + ": " + count));
 
     }
 }
